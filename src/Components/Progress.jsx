bf2004cc392b7addb52579d5adf1a72f994bb478
yasubdemissie <yasdam777@gmail.com>
2024-07-25 14:15:21 +0300

    third commit!

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,11 +1,11 @@
 import propTypes from 'prop-types';
 function Progress({ index, point, numberOfQuestions, numberOfPoints, answer }) {
     return (
-        <div className="progress">
+        <header className="progress">
             <progress max={numberOfQuestions} value={index + Number(answer !== null)} />
             <p>question: {index + 1} / {numberOfQuestions}</p>
             <p>points: {point} / {numberOfPoints}</p>
-        </div>
+        </header>
     )
 }
 
