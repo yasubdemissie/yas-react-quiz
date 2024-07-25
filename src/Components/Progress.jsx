c564c054a4634a338a556cea236722dcaed184ca
yasubdemissie <yasdam777@gmail.com>
2024-07-25 14:07:22 +0300

    second commit into the newly created branch

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,10 +1,10 @@
 function Progress({ index, point, numberOfQuestions, numberOfPoints, answer }) {
     return (
-        <div className="progress">
+        <header className="progress">
             <progress max={numberOfQuestions} value={index + Number(answer !== null)} />
             <p>question: {index + 1} / {numberOfQuestions}</p>
             <p>points: {point} / {numberOfPoints}</p>
-        </div>
+        </header>
     )
 }
 
