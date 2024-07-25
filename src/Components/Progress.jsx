a80edb54a29cb7d959136543d79ddf5324b63859
yasubdemissie <yasdam777@gmail.com>
2024-07-25 14:08:10 +0300

    second commit

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,10 +1,10 @@
 function Progress({ index, point, numberOfQuestions, numberOfPoints, answer }) {
     return (
-        <header className="progress">
+        <div className="progress">
             <progress max={numberOfQuestions} value={index + Number(answer !== null)} />
             <p>question: {index + 1} / {numberOfQuestions}</p>
             <p>points: {point} / {numberOfPoints}</p>
-        </header>
+        </div>
     )
 }
 
